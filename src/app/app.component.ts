141509b395a2e89f2c7b36de61234b579c69d257
Zbir <aminezbir09@gmail.com>
2025-03-02 16:00:45 +0000

    Add the passing state and loop

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,8 +5,14 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'tpAngular';
+  list = [
+    { nom: 'Zbir', prenom: 'Mohamed Amine', age: 23 },
+    { nom: 'Hanane', prenom: 'Salah', age: 24 },
+    { nom: 'Zbir', prenom: 'Sadika', age: 18 },
+    { nom: 'Zbir', prenom: 'Youssef', age: 25 },
+  ];
 }
