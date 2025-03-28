61e60ab8d7a66a6974d8357abf1fec189952d7d1
Mohamed Amine <amine339@outlook.fr>
2025-03-28 14:58:19 +0000

    Add the last parts of test functions for the persons service

--- src/app/services/persons.service.spec.ts
+++ src/app/services/persons.service.spec.ts
@@ -5,7 +5,8 @@ import { TestBed } from '@angular/core/testing';
 describe('person service tests',()=>{
     
     let staffService: PersonsService;
-    
+    let person: Persone;
+
     beforeEach(() => {
         
         TestBed.configureTestingModule({
@@ -23,10 +24,18 @@ describe('person service tests',()=>{
               {id: 5,nom: 'Durand',prenom: 'Lucie',age: 40,mature: true,salaire: 3500},
             ]
           )
+        /* for the add and delete part */
+        person = {
+            nom: "Zbir",
+            prenom: "Mohamed",
+            age: 23,
+            mature: true,
+            salaire: 4500
+        };
     });
 
 
-    describe('get all staff',()=>{
+    describe('Test getAll',()=>{
         
         it('returns the correct number of staff members inside my beforeeach fucntions',()=>{
             expect(staffService.getAllProducys().length).toEqual(5);
@@ -41,19 +50,9 @@ describe('person service tests',()=>{
         });
     });
 
-    describe('adding a user to the personState',()=>{
+    describe('Test Save',()=>{
         
-        let person: Persone;
-    
         beforeEach(() => {
-            person = {
-                nom: "Zbir",
-                prenom: "Mohamed",
-                age: 23,
-                mature: true,
-                salaire: 4500
-            };
-
             staffService.saveStaff(person);
         });
 
@@ -67,5 +66,38 @@ describe('person service tests',()=>{
         });
 
     })
+
+    describe('Test Delete',()=>{
+
+        beforeEach(()=>{
+            staffService.deleteStaff({id: 1,nom: 'Dupont',prenom: 'Jean',age: 35,mature: true,salaire: 3000})
+        })
+
+        it('returns 4 staff members after deleting the firs',()=>{
+            expect(staffService.getAllProducys().length).toEqual(4);
+        })
+
+        it('returns undefined when we look for dupont',()=>{
+            expect(staffService.getAllProducys().find(e => e.id == 1 )?.nom).toEqual(undefined)
+        })
+
+    });
+
+
+    describe('Test Select',()=>{
+
+        beforeEach(()=>{
+            staffService.select({id: 4,nom: 'Leclerc',prenom: 'Sophie',age: 22,mature: false,salaire: 1500})
+        });
+
+        it('returns 0 non mature',()=>{
+            expect(staffService.getAllProducys().filter(e => !e.mature ).length).toEqual(0);
+        });
+
+        it('returns true for staff memeber id 4',()=>{
+            expect(staffService.getAllProducys().find(e => e.id == 4)?.mature).toBe(true);
+        })
+
+    });
 
 });
